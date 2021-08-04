a88bdb4f007f42aa191b8df1474a51ceeeb5ddf0
Ilya <sotni-kov@mail.ru>
2021-08-05 01:41:27 +0300

    dist build 1.0

--- src/main-description.js
+++ src/main-description.js
@@ -9,6 +9,7 @@ let special = document.getElementById('15')
 let feedback = document.getElementById('16')
 let content = document.querySelector('.content-1')
 let list = [brand,device,price,contact,special,feedback]
+let lorem ='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda dignissimos ea facere magni, nisi obcaecati similique! Accusamus adipisci atque deserunt dolores dolorum ea eaque excepturi itaque modi obcaecati odio officia praesentium quidem, quis sed sint soluta sunt, vitae voluptas voluptate! Adipisci architecto consequatur doloribus ea excepturi explicabo hic incidunt ipsa iste molestias, natus nesciunt nihil placeat quod recusandae reiciendis, voluptas voluptates voluptatibus. A accusamus assumenda, commodi dolore earum, eligendi esse exercitationem, iure labore nostrum numquam officia quae recusandae totam voluptates. Asperiores laborum magnam molestias pariatur ratione recusandae! A earum esse impedit provident quaerat voluptates. Asperiores consectetur explicabo iusto quos?\n'
 
 console.log(list)
 console.log(content)
@@ -29,29 +30,27 @@ brand.addEventListener('click', function () {
     brand.classList.add('list__item_active')
 })
 device.addEventListener('click', function () {
-    content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid amet ducimus enim eveniet exercitationem fuga impedit in maxime, modi molestiae necessitatibus odio officiis placeat qui quis quos rem, sint sunt unde velit voluptas voluptatibus voluptatum? A consectetur ducimus eum, ipsum labore minima nisi odit porro quaerat quasi sint vitae.\n'
+    content.innerHTML = lorem
     remove()
     device.classList.add('list__item_active')
 })
 price.addEventListener('click', function () {
-    content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid amet ducimus enim eveniet exercitationem fuga impedit in maxime, modi molestiae necessitatibus odio officiis placeat qui quis quos rem, sint sunt unde velit voluptas voluptatibus voluptatum? A consectetur ducimus eum, ipsum labore minima nisi odit porro quaerat quasi sint vitae.\n'
-
+    content.innerHTML = lorem
     remove()
     price.classList.add('list__item_active')
 })
 contact.addEventListener('click', function () {
-    content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid amet ducimus enim eveniet exercitationem fuga impedit in maxime, modi molestiae necessitatibus odio officiis placeat qui quis quos rem, sint sunt unde velit voluptas voluptatibus voluptatum? A consectetur ducimus eum, ipsum labore minima nisi odit porro quaerat quasi sint vitae.\n'
-
+    content.innerHTML = lorem
     remove()
     contact.classList.add('list__item_active')
 })
 special.addEventListener('click', function () {
-    content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid amet ducimus enim eveniet exercitationem fuga impedit in maxime, modi molestiae necessitatibus odio officiis placeat qui quis quos rem, sint sunt unde velit voluptas voluptatibus voluptatum? A consectetur ducimus eum, ipsum labore minima nisi odit porro quaerat quasi sint vitae.\n'
+    content.innerHTML = lorem
     remove()
     special.classList.add('list__item_active')
 })
 feedback.addEventListener('click', function () {
-    content.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid amet ducimus enim eveniet exercitationem fuga impedit in maxime, modi molestiae necessitatibus odio officiis placeat qui quis quos rem, sint sunt unde velit voluptas voluptatibus voluptatum? A consectetur ducimus eum, ipsum labore minima nisi odit porro quaerat quasi sint vitae.\n'
+    content.innerHTML = lorem
     remove()
     feedback.classList.add('list__item_active')
 })
@@ -70,12 +69,21 @@ feedback.addEventListener('click', function () {
 
 
 let description = document.querySelector('.text-img-description__description')
-let descriptionAllButton = document.querySelector('.text-img-description__button')
+let descriptionAllButton = document.querySelector('.text-img-description__button--open')
+let descriptionHideButton = document.querySelector('.text-img-description__button--close')
+
 
 
 descriptionAllButton.addEventListener('click', function () {
     description.classList.add('text-img-description__description--all')
     descriptionAllButton.classList.add('text-img-description__button--remove')
+    descriptionHideButton.classList.remove('text-img-description__button--remove')
+})
+
+descriptionHideButton.addEventListener('click', function () {
+    description.classList.remove('text-img-description__description--all')
+    descriptionAllButton.classList.remove('text-img-description__button--remove')
+    descriptionHideButton.classList.add('text-img-description__button--remove')
 })
 
 
--- src/side-menu.js
+++ src/side-menu.js
@@ -15,3 +15,80 @@ exitButton.addEventListener('click', function () {
     exitButton.classList.remove('side-menu-top__exit-btn--enable')
 
 })
+
+
+let sideMenuList = document.querySelectorAll('.side-menu-list__item')
+let sideMenuRepair = sideMenuList[0];
+let sideMenuServices = sideMenuList[1];
+let sideMenuClients = sideMenuList[2];
+let sideMenuTraders = sideMenuList[3];
+let sideMenuPartners = sideMenuList[4];
+let sideMenuManufacturer = sideMenuList[5];
+let sideMenuCenters = sideMenuList[6];
+let sideMenuContacts = sideMenuList[7];
+
+let sideRemove = function () {
+    for (let i = 0 ; i<sideMenuList.length; i++) {
+        if (sideMenuList[i].classList.contains('side-menu-list__item_active')) {
+            sideMenuList[i].classList.remove('side-menu-list__item_active')
+        }
+    }
+}
+
+sideMenuRepair.addEventListener('click', function () {
+    sideRemove()
+    sideMenuRepair.classList.add('side-menu-list__item_active')
+})
+sideMenuServices.addEventListener('click', function () {
+    sideRemove()
+    sideMenuServices.classList.add('side-menu-list__item_active')
+})
+sideMenuClients.addEventListener('click', function () {
+    sideRemove()
+    sideMenuClients.classList.add('side-menu-list__item_active')
+})
+sideMenuTraders.addEventListener('click', function () {
+    sideRemove()
+    sideMenuTraders.classList.add('side-menu-list__item_active')
+})
+sideMenuManufacturer.addEventListener('click', function () {
+    sideRemove()
+    sideMenuManufacturer.classList.add('side-menu-list__item_active')
+})
+sideMenuPartners.addEventListener('click', function () {
+    sideRemove()
+    sideMenuPartners.classList.add('side-menu-list__item_active')
+})
+sideMenuCenters.addEventListener('click', function () {
+    sideRemove()
+    sideMenuCenters.classList.add('side-menu-list__item_active')
+})
+sideMenuContacts.addEventListener('click', function () {
+    sideRemove()
+    sideMenuContacts.classList.add('side-menu-list__item_active')
+})
+
+let sideMenuRU = document.querySelector('.side-menu-bottom__ru')
+let sideMenuEN = document.querySelector('.side-menu-bottom__en')
+let sideMenuCH = document.querySelector('.side-menu-bottom__ch')
+let sideMenuLang = [sideMenuRU,sideMenuEN, sideMenuCH]
+
+let sideRemoveLang = function () {
+    for (let i = 0 ; i<sideMenuLang.length; i++) {
+        if (sideMenuLang[i].classList.contains('side-menu-bottom_active')) {
+            sideMenuLang[i].classList.remove('side-menu-bottom_active')
+        }
+    }
+}
+sideMenuRU.addEventListener('click', function () {
+    sideRemoveLang()
+    sideMenuRU.classList.add('side-menu-bottom_active')
+})
+sideMenuEN.addEventListener('click', function () {
+    sideRemoveLang()
+    sideMenuEN.classList.add('side-menu-bottom_active')
+})
+sideMenuCH.addEventListener('click', function () {
+    sideRemoveLang()
+    sideMenuCH.classList.add('side-menu-bottom_active')
+})
